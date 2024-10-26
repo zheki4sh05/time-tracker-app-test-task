a83d6df35ab62e59be2d9a931fe1f7c5c91b9067
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-10-26 15:39:58 +0300

    refactoring dockerfile and docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
-FROM adoptopenjdk/openjdk11:alpine-jre
-RUN adduser --system spring-boot && addgroup --system spring-boot && adduser spring-boot spring-boot
-USER spring-boot
-WORKDIR /app
-COPY target/dependency ./lib
-COPY target/time-tracker-app-test-task-0.0.1-SNAPSHOT.jar ./application.jar
-EXPOSE 8080
-ENTRYPOINT ["java", "-cp", "./lib/*:./application.jar","com.timetrackerapptesttask.TimeTrackerAppTestTaskApplication"]
+
+FROM openjdk:17
+ARG JAR_FILE=target/time-tracker-app-test-task-0.0.1-SNAPSHOT.jar
+WORKDIR /opt/app
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","app.jar"]
+
--- src/main/resources/liquibase/changesets/V1__init.sql
+++ src/main/resources/liquibase/changesets/V1__init.sql
@@ -1,4 +1,24 @@
 
+--
+
+CREATE SCHEMA IF NOT EXISTS public AUTHORIZATION postgres;
+
+
+ALTER SCHEMA public OWNER TO pg_database_owner;
+
+--
+-- TOC entry 4894 (class 0 OID 0)
+-- Dependencies: 4
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: pg_database_owner
+--
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+--
+-- TOC entry 222 (class 1259 OID 17163)
+-- Name: activity; Type: TABLE; Schema: public; Owner: postgres
+--
+
 CREATE TABLE public.activity (
     started timestamp without time zone,
     finished timestamp without time zone,
@@ -11,6 +31,10 @@ CREATE TABLE public.activity (
 
 ALTER TABLE public.activity OWNER TO postgres;
 
+--
+-- TOC entry 223 (class 1259 OID 17168)
+-- Name: activity_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
 
 CREATE SEQUENCE public.activity_id_seq
     AS integer
@@ -24,8 +48,8 @@ CREATE SEQUENCE public.activity_id_seq
 ALTER SEQUENCE public.activity_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4878 (class 0 OID 0)
--- Dependencies: 228
+-- TOC entry 4895 (class 0 OID 0)
+-- Dependencies: 223
 -- Name: activity_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -33,7 +57,13 @@ ALTER SEQUENCE public.activity_id_seq OWNED BY public.activity.id;
 
 
 --
--- TOC entry 227 (class 1259 OID 16943)
+-- TOC entry 220 (class 1259 OID 17153)
+-- Name: databasechangelog; Type: TABLE; Schema: public; Owner: postgres
+--
+
+
+--
+-- TOC entry 224 (class 1259 OID 17169)
 -- Name: order_user_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -49,8 +79,8 @@ CREATE SEQUENCE public.order_user_seq
 ALTER SEQUENCE public.order_user_seq OWNER TO postgres;
 
 --
--- TOC entry 4879 (class 0 OID 0)
--- Dependencies: 227
+-- TOC entry 4896 (class 0 OID 0)
+-- Dependencies: 224
 -- Name: order_user_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -58,7 +88,7 @@ ALTER SEQUENCE public.order_user_seq OWNED BY public.activity.worker;
 
 
 --
--- TOC entry 216 (class 1259 OID 16816)
+-- TOC entry 225 (class 1259 OID 17170)
 -- Name: participation; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -73,7 +103,22 @@ CREATE TABLE public.participation (
 ALTER TABLE public.participation OWNER TO postgres;
 
 --
--- TOC entry 218 (class 1259 OID 16838)
+-- TOC entry 215 (class 1259 OID 16911)
+-- Name: participation_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.participation_seq
+    START WITH 1
+    INCREMENT BY 50
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.participation_seq OWNER TO postgres;
+
+--
+-- TOC entry 226 (class 1259 OID 17173)
 -- Name: project; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -87,7 +132,22 @@ CREATE TABLE public.project (
 ALTER TABLE public.project OWNER TO postgres;
 
 --
--- TOC entry 215 (class 1259 OID 16809)
+-- TOC entry 216 (class 1259 OID 16912)
+-- Name: project_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.project_seq
+    START WITH 1
+    INCREMENT BY 50
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.project_seq OWNER TO postgres;
+
+--
+-- TOC entry 227 (class 1259 OID 17178)
 -- Name: role; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -101,7 +161,22 @@ CREATE TABLE public.role (
 ALTER TABLE public.role OWNER TO postgres;
 
 --
--- TOC entry 220 (class 1259 OID 16869)
+-- TOC entry 217 (class 1259 OID 16913)
+-- Name: role_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.role_seq
+    START WITH 1
+    INCREMENT BY 50
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.role_seq OWNER TO postgres;
+
+--
+-- TOC entry 228 (class 1259 OID 17183)
 -- Name: token; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -117,7 +192,37 @@ CREATE TABLE public.token (
 ALTER TABLE public.token OWNER TO postgres;
 
 --
--- TOC entry 217 (class 1259 OID 16821)
+-- TOC entry 218 (class 1259 OID 16914)
+-- Name: token_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.token_seq
+    START WITH 1
+    INCREMENT BY 50
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.token_seq OWNER TO postgres;
+
+--
+-- TOC entry 219 (class 1259 OID 16915)
+-- Name: user_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.user_seq
+    START WITH 1
+    INCREMENT BY 50
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.user_seq OWNER TO postgres;
+
+--
+-- TOC entry 229 (class 1259 OID 17188)
 -- Name: worker; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -134,7 +239,7 @@ CREATE TABLE public.worker (
 ALTER TABLE public.worker OWNER TO postgres;
 
 --
--- TOC entry 226 (class 1259 OID 16916)
+-- TOC entry 230 (class 1259 OID 17193)
 -- Name: worker_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -150,8 +255,8 @@ CREATE SEQUENCE public.worker_id_seq
 ALTER SEQUENCE public.worker_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4880 (class 0 OID 0)
--- Dependencies: 226
+-- TOC entry 4897 (class 0 OID 0)
+-- Dependencies: 230
 -- Name: worker_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -159,7 +264,7 @@ ALTER SEQUENCE public.worker_id_seq OWNED BY public.worker.id;
 
 
 --
--- TOC entry 4710 (class 2604 OID 16944)
+-- TOC entry 4723 (class 2604 OID 17194)
 -- Name: activity worker; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -167,7 +272,7 @@ ALTER TABLE ONLY public.activity ALTER COLUMN worker SET DEFAULT nextval('public
 
 
 --
--- TOC entry 4711 (class 2604 OID 16972)
+-- TOC entry 4724 (class 2604 OID 17195)
 -- Name: activity id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -175,7 +280,7 @@ ALTER TABLE ONLY public.activity ALTER COLUMN id SET DEFAULT nextval('public.act
 
 
 --
--- TOC entry 4709 (class 2604 OID 16917)
+-- TOC entry 4725 (class 2604 OID 17196)
 -- Name: worker id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -183,7 +288,7 @@ ALTER TABLE ONLY public.worker ALTER COLUMN id SET DEFAULT nextval('public.worke
 
 
 --
--- TOC entry 4723 (class 2606 OID 16904)
+-- TOC entry 4737 (class 2606 OID 17198)
 -- Name: token Token_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -192,7 +297,7 @@ ALTER TABLE ONLY public.token
 
 
 --
--- TOC entry 4721 (class 2606 OID 16979)
+-- TOC entry 4729 (class 2606 OID 17200)
 -- Name: activity activity_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -201,7 +306,15 @@ ALTER TABLE ONLY public.activity
 
 
 --
--- TOC entry 4715 (class 2606 OID 16820)
+-- TOC entry 4727 (class 2606 OID 17162)
+-- Name: databasechangeloglock databasechangeloglock_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+
+
+
+--
+-- TOC entry 4731 (class 2606 OID 17202)
 -- Name: participation participation_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -210,7 +323,7 @@ ALTER TABLE ONLY public.participation
 
 
 --
--- TOC entry 4719 (class 2606 OID 16844)
+-- TOC entry 4733 (class 2606 OID 17204)
 -- Name: project project_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -219,7 +332,7 @@ ALTER TABLE ONLY public.project
 
 
 --
--- TOC entry 4713 (class 2606 OID 16891)
+-- TOC entry 4735 (class 2606 OID 17206)
 -- Name: role role_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -228,7 +341,7 @@ ALTER TABLE ONLY public.role
 
 
 --
--- TOC entry 4717 (class 2606 OID 16924)
+-- TOC entry 4739 (class 2606 OID 17208)
 -- Name: worker worker_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -237,7 +350,7 @@ ALTER TABLE ONLY public.worker
 
 
 --
--- TOC entry 4724 (class 2606 OID 16892)
+-- TOC entry 4742 (class 2606 OID 17209)
 -- Name: participation participation_role_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -246,7 +359,7 @@ ALTER TABLE ONLY public.participation
 
 
 --
--- TOC entry 4727 (class 2606 OID 16956)
+-- TOC entry 4740 (class 2606 OID 17214)
 -- Name: activity project_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -255,7 +368,7 @@ ALTER TABLE ONLY public.activity
 
 
 --
--- TOC entry 4725 (class 2606 OID 16961)
+-- TOC entry 4743 (class 2606 OID 17219)
 -- Name: participation project_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -264,7 +377,7 @@ ALTER TABLE ONLY public.participation
 
 
 --
--- TOC entry 4729 (class 2606 OID 16936)
+-- TOC entry 4745 (class 2606 OID 17224)
 -- Name: token user-fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -273,7 +386,7 @@ ALTER TABLE ONLY public.token
 
 
 --
--- TOC entry 4726 (class 2606 OID 16966)
+-- TOC entry 4744 (class 2606 OID 17229)
 -- Name: participation user_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -282,7 +395,7 @@ ALTER TABLE ONLY public.participation
 
 
 --
--- TOC entry 4728 (class 2606 OID 16951)
+-- TOC entry 4741 (class 2606 OID 17234)
 -- Name: activity worker_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -290,7 +403,7 @@ ALTER TABLE ONLY public.activity
     ADD CONSTRAINT worker_fk FOREIGN KEY (worker) REFERENCES public.worker(id) NOT VALID;
 
 
--- Completed on 2024-10-25 19:41:54
+-- Completed on 2024-10-26 15:20:49
 
 --
 -- PostgreSQL database dump complete
